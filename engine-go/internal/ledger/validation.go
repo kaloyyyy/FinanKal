83e3b9d3eac953d8a98997b0e48a9bbde4311f74
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2026-08-05 03:05:11 +0800

    SP3 (core): rename validator to validation

--- engine-go/internal/ledger/validation.go
+++ engine-go/internal/ledger/validation.go
@@ -4,20 +4,19 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/kaloy/finankal/engine-go/internal/model"
 	"github.com/shopspring/decimal"
 )
 
-func validateEntries(entries []model.Entry) error {
+func validateEntries(entries []Entry) error {
 	debit := decimal.Zero
 	credit := decimal.Zero
 
 	for _, e := range entries {
 		fmt.Println("Validating entry:", e.AccountID, e.Amount, e.Type)
 		switch e.Type {
-		case model.DEBIT:
+		case DEBIT:
 			debit = debit.Add(e.Amount)
-		case model.CREDIT:
+		case CREDIT:
 			credit = credit.Add(e.Amount)
 		default:
 			return errors.New("invalid entry type")
